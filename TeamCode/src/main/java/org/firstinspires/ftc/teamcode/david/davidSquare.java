9343bd21e2ba1711f93a5dc57173a60aed6c4234
Elijah <eporras@alberton.k12.mt.us>
2025-05-21 10:53:11 -0600

    David 10.10

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/david/davidSquare.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/david/davidSquare.java
@@ -1,32 +1,29 @@
 package org.firstinspires.ftc.teamcode.david;
 
-public class davidSquare {
-}
-// simple autonomous program that drives bot in a square pattern then ends.
-// this code assumes it will end before the period is over but if the period ended while
-// still driving, this code would just stop.
-
-package org.firstinspires.ftc.teamcode;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-@Autonomous(name="Drive Square", group="Exercises")
+@Autonomous(name="davidSquare", group="Exercises")
 //@Disabled
-public class DriveSquare extends LinearOpMode
+public class davidSquare extends LinearOpMode
 {
-    DcMotor leftMotor;
-    DcMotor rightMotor;
+    DcMotor leftFrontDrive;
+    DcMotor rightFrontDrive;
+    DcMotor leftRearDrive;
+    DcMotor rightRearDrive;
 
     // called when init button is  pressed.
     @Override
     public void runOpMode() throws InterruptedException
     {
-        leftMotor = hardwareMap.dcMotor.get("left_motor");
-        rightMotor = hardwareMap.dcMotor.get("right_motor");
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        leftRearDrive = hardwareMap.get(DcMotor.class, "left_rear_drive");
+        rightRearDrive = hardwareMap.get(DcMotor.class, "right_rear_drive");
 
-        leftMotor.setDirection(DcMotor.Direction.REVERSE);
+        leftFrontDrive.setDirection(DcMotor.Direction.REVERSE);
+        leftRearDrive.setDirection(DcMotor.Direction.REVERSE);
 
         telemetry.addData("Mode", "waiting");
         telemetry.update();
@@ -47,32 +44,43 @@ public class DriveSquare extends LinearOpMode
             telemetry.addData("Mode", "driving side " + (i + 1));
             telemetry.update();
 
-            leftMotor.setPower(0.25);
-            rightMotor.setPower(0.25);
+            leftFrontDrive.setPower(0.25);
+            rightFrontDrive.setPower(0.25);
+            leftRearDrive.setPower(0.25);
+            rightRearDrive.setPower(0.25);
 
             sleep(1000); // drive straight for 1 second.
 
-            leftMotor.setPower(0.0);
-            rightMotor.setPower(0.0);
+            leftFrontDrive.setPower(0);
+            rightFrontDrive.setPower(0);
+            leftRearDrive.setPower(0);
+            rightRearDrive.setPower(0);
 
             sleep(500);  // wait half second for bot to stop moving.
 
             // now set motors, one forward one reverse. Should cause the bot to rotate.
 
-            leftMotor.setPower(0.25);
-            rightMotor.setPower(-0.25);
+            leftFrontDrive.setPower(0.25);
+            rightFrontDrive.setPower(0.25);
+            leftRearDrive.setPower(-0.25);
+            rightRearDrive.setPower(-0.25);
+
 
             sleep(1700); // adjust this delay to get the bot to rotate 90 degrees.
 
-            leftMotor.setPower(0.0);
-            rightMotor.setPower(0.0);
+            leftFrontDrive.setPower(0.25);
+            rightFrontDrive.setPower(0.25);
+            leftRearDrive.setPower(0.25);
+            rightRearDrive.setPower(0.25);
 
             sleep(500); // wait for bot to stop moving.
         }
 
         // make sure the motors are off.
 
-        rightMotor.setPower(0);
-        leftMotor.setPower(0);
+        leftFrontDrive.setPower(0);
+        rightFrontDrive.setPower(0);
+        leftRearDrive.setPower(0);
+        rightRearDrive.setPower(0);
     }
 }
